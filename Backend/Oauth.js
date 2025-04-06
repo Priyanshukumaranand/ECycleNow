5307152d74eccf723200fefb2dc0f05ed628d541
Priyanshu Kumar Anand <118902541+Priyanshukumaranand@users.noreply.github.com>
2025-04-06 23:33:43 +0530

    backend connectivity

--- Backend/Oauth.js
+++ Backend/Oauth.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_CALLBACK_URL = "http://localhost:8000/api/v1/auth/google/callback";
+export const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL_2;
